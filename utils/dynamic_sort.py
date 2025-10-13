357339ff640cae3916957fb96aa15240db9f4453
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-10-13 15:44:37 +0200

    Break out dynamic sort to its own file

--- utils/dynamic_sort.py
+++ utils/dynamic_sort.py
@@ -1,5 +1,10 @@
-class QuickSort:
-    def __init__(self) -> None:
+import math
+from utils.heap_sort import HeapSort
+
+
+class DynamicSort:
+    def __init__(self, max_depth=None) -> None:
+        self.max_depth = max_depth
         self.count = 0
 
     def _partition(self, list, low, high):
@@ -26,14 +31,23 @@ class QuickSort:
         return j
 
     def sort(self, list):
-        self._sort(list, 0, len(list) - 1)
+        if self.max_depth is None:
+            self.max_depth = int(2 * math.log2(len(list)))
+        self._sort(list, 0, len(list) - 1, 0)
 
-    def _sort(self, list, low, high):
+    def _sort(self, list, low, high, depth):
         if high <= low:
+            return
+
+        if depth >= self.max_depth:
+            print("Shiften to heap_sort")
+            heap_sort = HeapSort(list[low : high + 1])
+            heap_sort.sort()
+            list[low : high + 1] = heap_sort.height[1:]
             return
 
         self.count += 1
 
         j = self._partition(list, low, high)
-        self._sort(list, low, j - 1)
-        self._sort(list, j + 1, high)
+        self._sort(list, low, j - 1, depth + 1)
+        self._sort(list, j + 1, high, depth + 1)
