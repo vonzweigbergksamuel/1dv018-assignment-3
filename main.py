4969f39edbfa63af274d13bbf41620891ee48029
svz <143542619+vonzweigbergksamuel@users.noreply.github.com>
2025-10-13 14:35:21 +0200

    Implement quick sort with median-of-three

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+from utils.quick_sort import QuickSort
+
+
+def main():
+    list = []
+    for _ in range(1000):
+        list.append(random.randint(0, 1000))
+
+    # print("Unsorted list", list)
+
+    quick_sort = QuickSort()
+    quick_sort.sort(list)
+
+    print("Sorted function calls", quick_sort.count)
+    # print("Sorted list", list)
+
+
+if __name__ == "__main__":
+    main()
